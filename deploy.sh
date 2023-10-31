00d7a38f79acefd36801a88dc0afa484500cf708
Tyler Carbone <tcarbone073@gmail.com>
2023-10-31 00:25:13 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+# config files
+rm -f .bashrc
+rm -f .vimrc
+rm -f .gitconfig
+rm -f .tmux.conf
+ln -s dot_files/.bashrc .bashrc
+ln -s dot_files/.vimrc .vimrc
+ln -s dot_files/.gitconfig .gitconfig
+ln -s dot_files/.tmux.conf .tmux.conf
+
+# ~/.vim
+rm -rf .vim
+mkdir .vim
+mkdir .vim/colors
+mkdir .vim/syntax
+mkdir .vim/after
+mkdir .vim/after/syntax
+ln -s ~/dot_files/.vim/pack ~/.vim/pack
+
+# ~/vim-chs
+rm -rf vim-chs
+git clone git@github.com:twcarbone/vim-chs.git
+ln -s ~/vim-chs/syntax/python.vim .vim/syntax/python.vim
+ln -s ~/vim-chs/colors/chs.vim .vim/colors/chs.vim
+ln -s ~/vim-chs/after/syntax/python.vim .vim/after/syntax/python.vim
+
