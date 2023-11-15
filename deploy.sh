4272978abb6e93c537cd7a99f3568f5b030516d1
Tyler Carbone <tcarbone073@gmail.com>
2023-11-15 00:59:59 +0000

    (Closes #2) Do not wipe out vim-chs in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,11 @@ mkdir .vim/after/syntax
 ln -s ~/dot_files/.vim/pack ~/.vim/pack
 
 # ~/vim-chs
-rm -rf vim-chs
-git clone git@github.com:twcarbone/vim-chs.git
+if [ -d ~/vim-chs ]; then
+	git -C ~/vim-chs pull origin main
+else
+	git clone git@github.com:twcarbone/vim-chs.git
+fi
 ln -s ~/vim-chs/syntax/python.vim .vim/syntax/python.vim
 ln -s ~/vim-chs/colors/chs.vim .vim/colors/chs.vim
 ln -s ~/vim-chs/after/syntax/python.vim .vim/after/syntax/python.vim
