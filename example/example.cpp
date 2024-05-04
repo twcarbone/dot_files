e3c280e0b0e6625beabdc13c0e35460e56ec3ce2
Tyler Carbone <tcarbone073@gmail.com>
2024-05-04 07:43:38 -0400

    [clang-format] Refine #include ordering, add example

---
+++ example/example.cpp
@@ -0,0 +1,20 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+
+#include <initializer_list>
+#include <iostream>
+
+#include <QByteArray>
+#include <QString>
+#include <QStringList>
+
+#include <../RelativeExternalProjectHeader.h>
+#include <ExternalProjectHeader.h>
+
+#include "../RelativeLocalProjectHeader.h"
+#include "LocalProjectHeader.h"
+
+int main(void)
+{
+    std::cout << "Hello, world!" << std::endl;
+}
