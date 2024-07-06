2b38da3413fb2b34b8854386bfdf9a2b99409d8d
Tyler Carbone <tcarbone073@gmail.com>
2024-07-06 06:00:20 -0400

    [deploy] Fix redundant '.git' in fzf.vim.git plugin

--- deploy.sh
+++ deploy.sh
@@ -93,7 +93,7 @@ main()
 
     # Create repositories in ~/.vim/pack/plugins/start
     init_vim_plugin 'tpope/vim-commentary' 'vim-commentary'
-    init_vim_plugin 'junegunn/fzf.vim.git' 'vim-fzf'
+    init_vim_plugin 'junegunn/fzf.vim' 'vim-fzf'
 
     # Install python tools
     python3 -m venv .pytools
