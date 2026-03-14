cdf9accffa861f4757f43fb0c0cc47a33c748dbe
Tyler Carbone <tcarbone073@gmail.com>
2026-03-14 06:17:06 -0400

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+# C:\Users\<user>\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+#Set-PSReadLineOption -PredictionSource History
